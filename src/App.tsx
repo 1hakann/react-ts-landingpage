e35b27df3b9da309c794d2a959f135c343581cee
Hakan Çakmak <hakan.cakmak@live.com>
2024-03-10 01:59:06 +0300

    route structure was completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PropsWithChildren, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from './store';
+import { toggleDirection } from './store/themeConfigSlice';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ PropsWithChildren }: PropsWithChildren) {
+  const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    dispatch(toggleDirection(themeConfig.direction));
+  }, [dispatch, themeConfig.direction]);
+
+  return <div className={`App ${themeConfig.theme}`}>{children}</div>;
 }
 
 export default App
